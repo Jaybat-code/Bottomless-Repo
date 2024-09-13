b896104152e83d80d75d0207c2d197ccf3348ddc
Jaybat-code <jacobbattenfielddev@gmail.com>
2024-09-13 08:40:08 -0500

    Index Table Has Been Created And Is Repsonsive

---
+++ Index/app.js
@@ -0,0 +1,20 @@
+
+//collecting an array of the classes for easy medium and hard
+const easy = document.getElementsByClassName("easy");
+const medium = document.getElementsByClassName("medium");
+const hard = document.getElementsByClassName("hard");
+//accessing the span elements to get their innertext
+const numOfEasy = document.getElementById("numOfEasy");
+const numOfMed = document.getElementById("numOfMed");
+const numOfHard = document.getElementById("numOfHard");
+//function that whenever the document loads it sets the text as the current number of projects in categories
+function listenForLoad(){
+document.addEventListener("DOMContentLoaded",()=>{
+    numOfEasy.innerHTML=easy.length
+    numOfMed.innerHTML=medium.length
+    numOfHard.innerHTML=hard.length
+})
+}
+//the actual function call of listenForLoad()
+listenForLoad();
+
