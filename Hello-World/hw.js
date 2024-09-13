bf3768767acbdfa7bc458eb16e320fd2f72b3115
Jaybat-code <jacobbattenfielddev@gmail.com>
2024-09-13 10:06:18 -0500

    Code Complete + Slight Spaghetti

---
+++ Hello-World/hw.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",()=>{
+const helloWorld = document.createElement("h1");
+helloWorld.setAttribute("class","hello");
+helloWorld.innerText="Hello World"
+document.body.appendChild(helloWorld)
+})
+    
