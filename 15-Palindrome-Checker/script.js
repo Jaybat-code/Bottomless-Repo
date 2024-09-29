d7bed814fa40ceb2b1ef1712a24f0550e0e7e69a
Jaybat-code <jacobbattenfielddev@gmail.com>
2024-09-29 18:54:28 -0500

    testing stuff

--- 15-Palindrome-Checker/script.js
+++ 15-Palindrome-Checker/script.js
@@ -7,67 +7,27 @@ let cleanedString = "";
 const cleanText = (text) => {
     return text.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 };
-
-const checkIfEmpty=()=>{
+function checkIfPalindrome(){
     if(textInput.value===""){
-        alert("Please input a value");
+        alert("Please enter in valid text!");
+        return;
     }else{
-        cleanedString=cleanText(textInput.value);
-        evaluateTextLength();
+        cleanedString = cleanText(textInput.value)
     }
-}
-const evaluateTextLength=()=>{
-    let textLength = cleanedString.length;
-    if(textLength%2==0){
-        checkEvenPalindrome();
-    }else if(textLength%2==1){
-        checkOddPalindrome();
-    }
-}
-//we need to split the array in half, take the last half, put it all in lower case, reverse it and see if it is equal to the lowercase of the first part
-const checkEvenPalindrome = ()=>{
-    //getting the length of text, divding this even length by two and splitting it into an array of letters and numbers
-    let textLength = cleanedString.length;
-    let halfOfEvenPalindrome = textLength/2;
-    let textArray = cleanedString.split("");
-    //empty arrays for pushing
-    let lastHalfOfString = [];
-    let firstHalfOfString = [];
-    //pushes the last half of the string in the last array
-    for(let i = halfOfEvenPalindrome;i<textLength;i++){
-        lastHalfOfString.push(textArray[i])
-    }
-    //pushes the first half of the string in the first array
-    for(let i = 0;i<halfOfEvenPalindrome;i++){
-        firstHalfOfString.push(textArray[i])
-    }
-    //compares the two strings and inputs a result using a string literal
-    if(firstHalfOfString.toString()===lastHalfOfString.reverse().toString()){
-        result.innerHTML=`${textInput.value} is a palindrome`
-    }else{
-        result.innerHTML=`${textInput.value} is not a palindrome`
-    }
-}
-const checkOddPalindrome = ()=>{
-    let textLength = cleanedString.length;
-    let halfOfOddPalindrome = (textLength-1)/2;
-    let textArray = cleanedString.split("");
-    
-    let lastHalfOfString = [];
-    let firstHalfOfString = [];
+    //reversing a string manually
+    let normalString = cleanedString;
+    let reversedString = "";
 
-    for(let i =halfOfOddPalindrome+1;i<textLength;i++){
-        lastHalfOfString.push(textArray[i]);
+    for(let i =normalString.length;i>0;i--){
+        reversedString+=normalString[i-1];
     }
-
-    for(let i=0;i<halfOfOddPalindrome;i++){
-        firstHalfOfString.push(textArray[i]);
-    }
-    if(firstHalfOfString.toString()===lastHalfOfString.reverse().toString()){
-        result.innerHTML=`${textInput.value} is a palindrome`
-    }else{
-        result.innerHTML=`${textInput.value} is not a palindrome`
+    if(normalString===reversedString){
+        result.innerHTML=`${normalString} is a palindrome!`
+    }else if(normalString!==reversedString){
+        result.innerHTML=`${normalString} is not a palindrome!`
     }
 }
+
+
     
-checkBtn.addEventListener("click",checkIfEmpty);
+checkBtn.addEventListener("click",checkIfPalindrome);
